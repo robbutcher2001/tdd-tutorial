3d3e0db72115210aa2746ee41797b7c22635eb82
Rob Butcher <robbutcher2001@gmail.com>
2021-03-05 16:46:11 +0000

    UI integration test defs

---
+++ cypress/integration/text-transformer.spec.ts
@@ -0,0 +1,24 @@
+describe("Text Transformer", () => {
+  it("should load with an initially empty input box", () => {});
+  it("should load with disabled action buttons", () => {});
+  it("should load with an initially empty list before any button is clicked", () => {});
+  it("should let you type text into an input field", () => {});
+  it("should only enable action buttons once there is one or more characters typed into the input field", () => {});
+  it("should have four action buttons available 'Lowercase', 'Uppercase', 'Camel case' and 'Capitalise case'", () => {});
+  it("should show the transformation text result in an 'output card' once a button is clicked", () => {});
+  it("should contain an output card with the contents of 'original text', 'transformed text' and the 'action taken' by the user", () => {});
+  it("should contain a list of two cards once two action buttons have been clicked", () => {});
+  it("should contain a list of three cards once three action buttons have been clicked, with the most recent translation at the top", () => {});
+  it("should not maintain state between page (app) refreshes", () => {});
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+  it("", () => {});
+});
+
+export {};
